b43184f220c7f88d93fc543d4c06d2e457b4e486
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2017-05-04 13:18:05 +0900

    Create user signup. Add debug draw.

--- toy_app/lib/toy_app/web/views/layout_view.ex
+++ toy_app/lib/toy_app/web/views/layout_view.ex
@@ -1,3 +1,6 @@
 defmodule ToyApp.Web.LayoutView do
   use ToyApp.Web, :view
+
+  def get_controller_name(conn), do: controller_module(conn)
+  def get_action_name(conn), do: action_name(conn)
 end
--- toy_app/lib/toy_app/web/views/user_view.ex
+++ toy_app/lib/toy_app/web/views/user_view.ex
@@ -1,3 +1,10 @@
 defmodule ToyApp.Web.UserView do
   use ToyApp.Web, :view
+
+  alias ToyApp.Accounts.User
+  alias ToyApp.Helpers.Gravatar
+
+  def gravatar_for(%User{email: email}) do
+    Gravatar.get_gravatar_url(email, 50)
+  end
 end
